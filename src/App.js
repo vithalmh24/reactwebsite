58e4fbcadf3a4030980a05880e8ce7e70cd67ab2
vitthal <vitthal.mhadgut1997@gmail.com>
2023-08-15 20:53:10 +0530

    Map Function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@
 import './App.css';
 
 function App() {
+  const students=[
+    { Name : "Vitthal",Age : 25,IsSamePerson:true},
+    { Name : "Shubham",Age : 26,IsSamePerson:true},
+    { Name : "Akshay",Age : 28,IsSamePerson: true },
+
+  ]
   return (
+   
     <div className="App">
-      Hello React
+      {
+      students.map((user,key)=>{
+         if(user.IsSamePerson){
+          return <MyComponent Name={user.Name} Age={user.Age}  />;
+        }
+       
+      })
+      }
+    </div>
+  );
+}
+
+const MyComponent = (props)=>{
+  return(
+    <div>
+      <h2>{props.Name},{props.Age}</h2>
+      <h2>{props.Age}</h2>
+      <h2>{props.email}</h2>
+      <hr />
     </div>
   );
 }
